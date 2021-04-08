9f81fa7a251e356c66f9137fd936b57f0e592e0b
OscarGlez01 <60475217+OscarGlez01@users.noreply.github.com>
2021-04-07 23:02:41 -0700

    Create Periodo.java

---
+++ Celfon/Celfon/src/mx/itson/celfon/entidades/Periodo.java
@@ -0,0 +1,57 @@
+
+package mx.itson.celfon.entidades;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author osc04
+ */
+public class Periodo {
+    private int id;
+    private String telefono;
+    private int duracion;
+    private Date fecha;
+    private List<Llamada> llamadas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public List<Llamada> getLlamadas() {
+        return llamadas;
+    }
+
+    public void setLlamadas(List<Llamada> llamadas) {
+        this.llamadas = llamadas;
+    }
+}
