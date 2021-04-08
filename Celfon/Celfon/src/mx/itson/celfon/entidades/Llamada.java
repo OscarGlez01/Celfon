2ae9faa1055c545171eead83ce97aa4b39daf054
OscarGlez01 <60475217+OscarGlez01@users.noreply.github.com>
2021-04-07 23:02:34 -0700

    Create Llamada.java

---
+++ Celfon/Celfon/src/mx/itson/celfon/entidades/Llamada.java
@@ -0,0 +1,48 @@
+
+package mx.itson.celfon.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Oscar Gonzalez Leyva
+ */
+public class Llamada {
+    private int id;
+    private String telefono;
+    private int duracion;
+    private Date fecha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+}
