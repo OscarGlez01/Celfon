1ddcedbf6c35b1f5e0a44c591739dba8e9d55dc5
OscarGlez01 <60475217+OscarGlez01@users.noreply.github.com>
2021-04-07 23:02:18 -0700

    Create Cliente.java

---
+++ Celfon/Celfon/src/mx/itson/celfon/entidades/Cliente.java
@@ -0,0 +1,54 @@
+
+package mx.itson.celfon.entidades;
+
+/**
+ *
+ * @author Oscar González Leyva
+ */
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String direccion;
+    private Ciudad ciudad;
+    private String telefono;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
