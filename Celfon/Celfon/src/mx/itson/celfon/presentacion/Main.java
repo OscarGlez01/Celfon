92ec087183024b6a32126282f81053531c9a6805
OscarGlez01 <60475217+OscarGlez01@users.noreply.github.com>
2021-04-07 23:03:42 -0700

    Create Main.java

---
+++ Celfon/Celfon/src/mx/itson/celfon/presentacion/Main.java
@@ -0,0 +1,27 @@
+
+package mx.itson.celfon.presentacion;
+
+import java.util.List;
+import mx.itson.celfon.entidades.Estado;
+import mx.itson.celfon.persistencias.ClienteDAO;
+import mx.itson.celfon.persistencias.EstadoDAO;
+
+/**
+ *
+ * @author Oscar Gonzalez
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        List<Estado> estados = EstadoDAO.obtenerTodos();
+        
+        for (Estado e : estados){
+            System.out.println("Nombre "+ e.getNombre());
+        }
+        
+        //ClienteDAO.buscar("juan");
+    }
+    
+}
