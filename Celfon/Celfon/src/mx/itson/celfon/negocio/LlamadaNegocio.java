a76b45df5e4762c2ea505dc829adb56d771b36a0
OscarGlez01 <60475217+OscarGlez01@users.noreply.github.com>
2021-04-08 20:59:45 -0700

    Se añade la capa de negocio y se crea una clase de ejemplo

---
+++ Celfon/Celfon/src/mx/itson/celfon/negocio/LlamadaNegocio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.celfon.negocio;
+
+/**
+ *
+ * @author González
+ */
+public class LlamadaNegocio {
+    
+    public int validarDuracion(int result){
+    
+        return result;
+} 
+    
+}
