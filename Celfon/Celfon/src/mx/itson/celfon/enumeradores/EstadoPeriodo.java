84a7efca42f0ef71b4a4c0968a364cff84bc15f1
OscarGlez01 <60475217+OscarGlez01@users.noreply.github.com>
2021-04-08 20:58:58 -0700

    Se modifica el enumerador de estado

--- Celfon/Celfon/src/mx/itson/celfon/enumeradores/EstadoPeriodo.java
+++ Celfon/Celfon/src/mx/itson/celfon/enumeradores/EstadoPeriodo.java
@@ -7,5 +7,23 @@ package mx.itson.celfon.enumeradores;
  */
 public enum EstadoPeriodo {
     
-        PENDIENTE, PAGADO, CANCELADO;
+        PENDIENTE(1), PAGADO(2), CANCELADO(3);
+        
+        private int valor;
+        
+        EstadoPeriodo(int valor){
+            this.valor = valor;
+        }
+        
+        public static EstadoPeriodo obtenerPorNumero(int valor){
+            switch(valor){
+                case 1:
+                    return PENDIENTE;
+                case 2:
+                    return PAGADO;
+                case 3:
+                    return CANCELADO;
+            }
+            return null;
+        }
 }
