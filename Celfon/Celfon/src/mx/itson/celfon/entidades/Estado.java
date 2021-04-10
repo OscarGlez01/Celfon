30ab3e140657814b1f092a022f7fb0e1bb4a5889
JesusLope619 <jesus.lopez216674@potros.itson.edu.mx>
2021-04-10 15:05:01 -0700

    Update Estado.java

--- Celfon/Celfon/src/mx/itson/celfon/entidades/Estado.java
+++ Celfon/Celfon/src/mx/itson/celfon/entidades/Estado.java
@@ -9,18 +9,31 @@ public class Estado {
     private int id;
     private String nombre;
 
+    /**
+     * Obtiene el valor de Id
+     * @return Devuelve Id
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Pone el valor de Id
+     * @param id 
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    /**
+     * Obtiene el valor de Nombre
+     * @return Devuelve Nombre
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * Pone el valor de Nombre
+     * @param nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
