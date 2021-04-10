df374ba10c2c552758a2555814ba8c668e115971
JesusLope619 <jesus.lopez216674@potros.itson.edu.mx>
2021-04-10 15:04:51 -0700

    Update Ciudad.java

--- Celfon/Celfon/src/mx/itson/celfon/entidades/Ciudad.java
+++ Celfon/Celfon/src/mx/itson/celfon/entidades/Ciudad.java
@@ -11,26 +11,45 @@ public class Ciudad {
     private String nombre;
     private Estado estado;
 
+    /**
+     * Obtiene el valor de Id
+     * @return Devuelve Id
+     */
     public int getId() {
         return id;
     }
-
+    /**
+    * Pone el valor de Id
+    * @param id 
+    */
     public void setId(int id) {
         this.id = id;
     }
-
+    /**
+    * Obtiene el valor de nombre
+    * @return 
+    */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * Pone el valor de Nombre
+     * @param nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * Obtiene el valor de Estado
+     * @return Devuelve estado
+     */
     public Estado getEstado() {
         return estado;
     }
-
+    /**
+     * Pone el valor de Estado
+     * @param estado 
+     */
     public void setEstado(Estado estado) {
         this.estado = estado;
     }
