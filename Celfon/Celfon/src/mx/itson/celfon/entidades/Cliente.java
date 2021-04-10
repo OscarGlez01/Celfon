8d39e75bf16ccf37652be29796f554b274cd8933
JesusLope619 <jesus.lopez216674@potros.itson.edu.mx>
2021-04-10 15:04:56 -0700

    Update Cliente.java

--- Celfon/Celfon/src/mx/itson/celfon/entidades/Cliente.java
+++ Celfon/Celfon/src/mx/itson/celfon/entidades/Cliente.java
@@ -13,42 +13,73 @@ public class Cliente {
     private Ciudad ciudad;
     private String telefono;
 
+    /**
+     * Obtener el valor de Id
+     * @return Devuelve Id
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Pone el valor de Id
+     * @param id 
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    /**
+     * Obtiene el valor de Nombre
+     * @return 
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * Pone el valor de nombre
+     * @param nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * Obtiene el valor de Direccion
+     * @return Devuelve Direccion
+     */
     public String getDireccion() {
         return direccion;
     }
-
+    /**
+     * Pone el valor de Direccion
+     * @param direccion 
+     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
-
+    /**
+     * Obtiene el valor de Ciudad
+     * @return Devuelve Ciudad
+     */
     public Ciudad getCiudad() {
         return ciudad;
     }
-
+    /**
+     * Pone el valor de Ciudad
+     * @param ciudad 
+     */
     public void setCiudad(Ciudad ciudad) {
         this.ciudad = ciudad;
     }
-
+    /**
+     * Obtiene el valor de Telefono
+     * @return Devuelve Telefono
+     */
     public String getTelefono() {
         return telefono;
     }
-
+    /**
+     * Pone el valor de Telefono
+     * @param telefono 
+     */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
