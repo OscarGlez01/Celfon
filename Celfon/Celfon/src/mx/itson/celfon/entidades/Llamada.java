aa2012f90b1dbbfe59ba134b2df61436a0a4bbfb
JesusLope619 <jesus.lopez216674@potros.itson.edu.mx>
2021-04-10 15:05:07 -0700

    Update Llamada.java

--- Celfon/Celfon/src/mx/itson/celfon/entidades/Llamada.java
+++ Celfon/Celfon/src/mx/itson/celfon/entidades/Llamada.java
@@ -13,34 +13,59 @@ public class Llamada {
     private int duracion;
     private Date fecha;
 
+    /**
+     * Obtiene el valor de Id
+     * @return Devuelve Id
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Pone el valor de Id
+     * @param id 
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    /**
+     * Obtiene el valor de telefono
+     * @return Devuelve Telefono
+     */
     public String getTelefono() {
         return telefono;
     }
-
+    /**
+     * Pone el valor de telefono 
+     * @param telefono 
+     */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
-
+    /**
+     * Obtiene el valor de Duracion
+     * @return Devuelve Duracion
+     */
     public int getDuracion() {
         return duracion;
     }
-
+    /**
+     * Pone el valor de Duracion
+     * @param duracion 
+     */
     public void setDuracion(int duracion) {
         this.duracion = duracion;
     }
-
+    /**
+     * Obtiene el valor para fecha
+     * @return Devuelve Fecha
+     */
     public Date getFecha() {
         return fecha;
     }
-
+    /**
+     * Pone el valor para Fecha
+     * @param fecha 
+     */
     public void setFecha(Date fecha) {
         this.fecha = fecha;
     }
