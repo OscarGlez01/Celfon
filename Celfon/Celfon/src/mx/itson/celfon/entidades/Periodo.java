cacd33f0b31791d1c57d299eab3a907681395045
JesusLope619 <jesus.lopez216674@potros.itson.edu.mx>
2021-04-10 15:05:12 -0700

    Update Periodo.java

--- Celfon/Celfon/src/mx/itson/celfon/entidades/Periodo.java
+++ Celfon/Celfon/src/mx/itson/celfon/entidades/Periodo.java
@@ -19,58 +19,101 @@ public class Periodo {
     private EstadoPeriodo estado;
     private List<Llamada> llamadas;
 
+    /**
+     * Obtiene el valor de Id
+     * @return Devuelve Id
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Pone el valor de Id
+     * @param id 
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    /**
+     * Obtiene valores para Llamadas
+     * @return Devuelve la lista de Llamadas
+     */
     public List<Llamada> getLlamadas() {
         return llamadas;
     }
-
+    /**
+     * Pone la lista de Llamadas
+     * @param llamadas 
+     */
     public void setLlamadas(List<Llamada> llamadas) {
         this.llamadas = llamadas;
     }
-
+    /**
+     * Obtiene el valor de Cliente
+     * @return Devuelve Cliente
+     */
     public Cliente getCliente() {
         return cliente;
     }
-
+    /**
+     * Pone el valor de Cliente
+     * @param cliente 
+     */
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
-
+    /**
+     * Obtiene el valor de Total
+     * @return Devuelve Total
+     */
     public double getTotal() {
         return total;
     }
-
+    /**
+     * Pone el valor de Total
+     * @param total 
+     */
     public void setTotal(double total) {
         this.total = total;
     }
-
+    /**
+     * Obtiene el valor de Mes
+     * @return Devuelve Mes
+     */
     public int getMes() {
         return mes;
     }
-
+    /**
+     * Pone el valor de Mes
+     * @param mes 
+     */
     public void setMes(int mes) {
         this.mes = mes;
     }
-
+    /**
+     * Obtiene el valor de Anio
+     * @return Devuelve Anio
+     */
     public int getAnio() {
         return anio;
     }
-
+    /**
+     * Pone el valor de Anio
+     * @param anio 
+     */
     public void setAnio(int anio) {
         this.anio = anio;
     }
-
+    /**
+     * Obtiene el valor de Estado
+     * @return Devuelve Estado
+     */
     public EstadoPeriodo getEstado() {
         return estado;
     }
-
+    /**
+     * Pone el valor de Estado
+     * @param estado 
+     */
     public void setEstado(EstadoPeriodo estado) {
         this.estado = estado;
     }
